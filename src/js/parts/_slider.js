f61f514a60ae28ce4c2b87e8b096fdf16008b1f9
tatynechka <tatynechka@mail.ru>
2020-05-22 23:33:42 +0300

    front-page html

--- src/js/parts/_slider.js
+++ src/js/parts/_slider.js
@@ -5,12 +5,7 @@
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    prevArrow: '<button type="button" class="slick-prev">Назад</button>',
-    nextArrow: '<button type="button" class="slick-next">Вперед</button>'
-  });
-
-  $('.slider-card').slick({
-    infinite: true,
+    dots: true,
     prevArrow: '<button type="button" class="slick-prev">Назад</button>',
     nextArrow: '<button type="button" class="slick-next">Вперед</button>'
   });
